8cce6db76155ed7a46e3de9a51f2a919433889cd
Muhammad Muhktar Musa <mcube3567@gmail.com>
2020-06-22 14:41:01 -0700

    day 10 of 100

---
+++ sum of all odd numbers .js
@@ -0,0 +1,20 @@
+function adOdd(n){
+   //n/2*(n + 1);
+   let sum= 0;
+   if (n <= 0 ){return }else{
+   return (2 * (n-1)) + 1}
+}
+function adOdd(n){
+    if (n <= 0 ){return }else{
+    return ((n + 1)/2)**2}
+ }
+ function wdOdd(n){
+     let sum =0;
+     for (i = 1; i < n; i++){
+         sum = ((i + 1)/2)**2
+     }
+     return sum
+ }                     
+ function wdOdd(n){
+    
+}    
